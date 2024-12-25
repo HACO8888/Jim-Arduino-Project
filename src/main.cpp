e610d39e3e3c6d1ea1e08c24b62b4fc5aeae7b7d
HACO <haco.tw@gmail.com>
2024-12-26 00:31:35 +0800

    feat: light

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
+int CDSPin = 0;
+int CDSVal = 0;
+int LEDPin = 5;
+
+
 void setup()
 {
+  Serial.begin(9600);
+
+  pinMode(LEDPin, OUTPUT);
 }
 
 void loop()
 {
+  CDSVal = analogRead(CDSPin);
+  Serial.println(CDSVal);
+  analogWrite(LEDPin, (CDSVal - 400 - 10) / 4);
 }
