3d4c8102a73b2b187ff220ec12e38827efcc0cf7
HACO <haco.tw@gmail.com>
2024-12-30 15:41:24 +0800

    feat: I2C LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 int CDSPin = 0;
 int CDSVal = 0;
 int LEDPin = 5;
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
   Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
 
   pinMode(LEDPin, OUTPUT);
 }
@@ -17,4 +22,8 @@ void loop()
   CDSVal = analogRead(CDSPin);
   Serial.println(CDSVal);
   analogWrite(LEDPin, (CDSVal - 400 - 10) / 4);
+  lcd.setCursor(0, 0);
+  lcd.print("Time Now:");
+  lcd.setCursor(1, 0);
+  lcd.print("");
 }
